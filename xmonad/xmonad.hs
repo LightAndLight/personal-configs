03bc335de1b311e73ffddfcd4447aa0c46ed580d
Isaac Elliott <isaace71295@gmail.com>
2017-03-28 14:33:40 +1000

    Added window navigation

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -2,11 +2,19 @@ import Data.Monoid
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Layout.WindowNavigation
+import XMonad.Util.EZConfig
 
 cfg = def
   { manageHook = manageDocks <+> manageHook def
-  , layoutHook  = avoidStruts $ layoutHook def
+  , layoutHook  = windowNavigation . avoidStruts $ layoutHook def
   , handleEventHook = handleEventHook def <> docksEventHook
   }
+  `additionalKeys`
+  [ ((mod1Mask, xK_l), sendMessage $ Go R)
+  , ((mod1Mask, xK_h), sendMessage $ Go L)
+  , ((mod1Mask, xK_j), sendMessage $ Go D)
+  , ((mod1Mask, xK_k), sendMessage $ Go U)
+  ]
 
 main = xmonad =<< xmobar cfg
