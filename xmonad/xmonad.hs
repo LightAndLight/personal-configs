d789a4e5e0cb926710a9cea176395ee5f4ab5ff7
Isaac Elliott <isaace71295@gmail.com>
2019-02-06 14:06:11 +1000

    Updated configs

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -4,18 +4,21 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.WindowNavigation
 import XMonad.Util.EZConfig
+import XMonad.Hooks.EwmhDesktops (ewmh)
+import System.Taffybar.Support.PagerHints (pagerHints)
 
 cfg = def
   { manageHook = manageDocks <+> manageHook def
   , layoutHook  = windowNavigation . avoidStruts $ layoutHook def
   , handleEventHook = handleEventHook def <> docksEventHook
+  , modMask = mod4Mask
   }
   `additionalKeys`
-  [ ((mod1Mask, xK_l), sendMessage $ Go R)
-  , ((mod1Mask, xK_h), sendMessage $ Go L)
-  , ((mod1Mask, xK_j), sendMessage $ Go D)
-  , ((mod1Mask, xK_k), sendMessage $ Go U)
-  , ((mod1Mask, xK_Return), spawn "xterm")
+  [ ((mod4Mask, xK_l), sendMessage $ Go R)
+  , ((mod4Mask, xK_h), sendMessage $ Go L)
+  , ((mod4Mask, xK_j), sendMessage $ Go D)
+  , ((mod4Mask, xK_k), sendMessage $ Go U)
+  , ((mod4Mask, xK_Return), spawn "urxvt")
   ]
 
-main = xmonad =<< xmobar cfg
+main = xmonad $ ewmh $ pagerHints cfg
