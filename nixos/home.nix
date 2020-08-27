7e74a418cc1744d13326ad3effbd6d08834639cf
Isaac Elliott <isaace71295@gmail.com>
2020-08-27 12:35:19 +1000

    Added spacemacs and fish to home.nix

--- nixos/home.nix
+++ nixos/home.nix
@@ -16,13 +16,35 @@ in
   imports = [
     home-manager.nixos
   ];
-
+  
+  users.users.isaac = {
+    isNormalUser = true;
+    shell = pkgs.fish;
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+  
   home-manager.users.isaac = { pkgs, ... }: {
+    home.file.".emacs.d" = { 
+      recursive = true; 
+      source = builtins.fetchGit {
+        name = "spacemacs";
+        url = https://github.com/syl20bnr/spacemacs/;
+        ref = "refs/heads/develop";
+        # `git ls-remote https://github.com/syl20bnr/spacemacs develop`
+        rev = "15d93914f5caf6a3a15c573da60576313b0eee04";
+      };
+    };
+    
     programs.fish.enable = true;
     programs.git = {
       enable = true;
       userName = "Isaac Elliott";
       userEmail = "isaace71295@gmail.com";
+      extraConfig = {
+        core = {
+	  editor = "nvim";
+	};
+      };
     };
   };
 }
