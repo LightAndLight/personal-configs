9a1421dd62e074c381bd7a8f3603dcf921cd25bd
Isaac Elliott <isaace71295@gmail.com>
2020-08-27 17:23:18 +1000

    nixos: enable upower when I'm on a laptop

--- nixos/default.nix
+++ nixos/default.nix
@@ -7,4 +7,16 @@
     ./packages.nix
     ./xserver.nix
   ];
+
+  options = with pkgs.lib; {
+    settings.isLaptop = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Enables laptop-related settings like battery levels and screen brightness";
+    };
+  };
+
+  config = {
+    services.upower.enable = config.settings.isLaptop;
+  };
 }
