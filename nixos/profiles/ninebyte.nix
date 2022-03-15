69224c9b2e7fbd460e4686f688e8666928658db6
Isaac Elliott <isaace71295@gmail.com>
2022-03-15 13:42:10 +1000

    add ssh match block for ninebyte gitlab

--- nixos/profiles/ninebyte.nix
+++ nixos/profiles/ninebyte.nix
@@ -1,5 +1,15 @@
-{ ... }: {
+{ config, ... }: {
   programs.git = {
     userEmail = "isaac@ninebyte.com";
+  };
+
+  programs.ssh = {
+    enable = true;
+    matchBlocks.ninebyte-gitlab = {
+      hostname = "gitlab.com";
+      user = "git";
+      identityFile = "${config.home.homeDirectory}/.ssh/ninebyte";
+      identitiesOnly = true;
+    };
   };
 }
