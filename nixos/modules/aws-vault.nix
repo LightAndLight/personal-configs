c4e77ceda3466e9776690f5905377f7fd2c5a4a2
Isaac Elliott <isaace71295@gmail.com>
2022-10-06 14:07:37 +1000

    add region option to aws-vault module

--- nixos/modules/aws-vault.nix
+++ nixos/modules/aws-vault.nix
@@ -10,6 +10,11 @@ in
         description = "The IAM user to authenticate as.";
       };
 
+      region = lib.mkOption {
+        type = lib.types.str;
+        description = "The region in which to authenticate.";
+      };
+
       mfaSerial = lib.mkOption {
         type = lib.types.str;
         description = "Serial number of the user's assigned MFA device.";
@@ -29,6 +34,11 @@ in
     ] ++
     (if awsVaultConfig.promptMethod == "zenity"
       # `zenity` allows `aws-vault` to open graphical dialog for MFA code entry.
+      #
+      # `zenity` requires access to the GNOME Keyring. I wasn't able to get Home Manager
+      # to install GNOME Keyring, so you need to install it separately.
+      #
+      # On NixOS, add `services.gnome.gnome-keyring.enable = true;` to your `configuration.nix`.
       then [ gnome.zenity ]
       else []
     );
@@ -37,10 +47,12 @@ in
       recursive = true;
       text = ''
         [default]
+        region = ${awsVaultConfig.region}
         credential_process = aws-vault exec ${awsVaultConfig.iamUser} --duration=8h --json --prompt=${awsVaultConfig.promptMethod}
 
         [profile ${awsVaultConfig.iamUser}]
         mfa_serial = ${awsVaultConfig.mfaSerial}
+        region = ${awsVaultConfig.region}
       '';
     };
 
