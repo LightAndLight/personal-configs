7a5a95a21aa3338da0d8211f3f5ac1ae7c703eeb
Isaac Elliott <isaace71295@gmail.com>
2023-05-28 08:52:32 +1000

    add more helix configuration

--- nixos/profiles/common.nix
+++ nixos/profiles/common.nix
@@ -122,10 +122,18 @@
       insert = "bar";
       select = "underline";
     };
+    # requires 23.03?
+    # settings.editor.text-width = 100;
     settings.editor.lsp = {
       display-messages = true;
       # requires 23.03
       # display-inlay-hints = true;
+    };
+    settings.keys = {
+      select = {
+        "$" = "extend_to_line_end";
+        "^" = "extend_to_line_start";
+      };
     };
   };
 
