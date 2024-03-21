f25d6b5dae20c8aacf6494248c0f460f01231cc7
Isaac Elliott <isaace71295@gmail.com>
2024-03-22 06:56:50 +1000

    enable SSH connection persistence

--- nixos/profiles/common.nix
+++ nixos/profiles/common.nix
@@ -106,6 +106,17 @@
     };
   };
 
+  programs.ssh = {
+    enable = true;
+    controlPersist = "60m";
+    controlMaster = "auto";
+    controlPath = "~/.ssh/%u@%l-%r@%h:%p.sock";
+    matchBlocks."github.com" = {
+      identityFile = "${config.home.homeDirectory}/.ssh/id_ed25519";
+      extraOptions.PreferredAuthentications = "publickey";
+    };
+  };
+
   programs.git = {
     enable = true;
     lfs.enable = true;
