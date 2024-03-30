837ade6eeb07472d812e0da6e18a96a919c4018a
Isaac Elliott <isaace71295@gmail.com>
2024-03-30 15:19:31 +1000

    Pin spacemacs and spacemacs-neuron in flake.nix

--- flake.nix
+++ flake.nix
@@ -8,8 +8,18 @@
       url = "github:morhetz/gruvbox-contrib?rev=edb3ee5f626cdfb250d5ab42c1f5ccb9f8050514";
       flake = false;
     };
+
+    spacemacs = {
+      url = "github:syl20bnr/spacemacs?rev=6774f37fc1c542fab664f1b65931e52a4991d71a";
+      flake = false;
+    };
+
+    spacemacs-neuron = {
+      url = "github:LightAndLight/spacemacs-neuron?rev=3750b3e7793f50e11ffac3aa0425a56fc279d5ab";
+      flake = false;
+    };
   };
-  outputs = { self, nixpkgs, home-manager, gruvbox-contrib }: {
+  outputs = { self, nixpkgs, home-manager, gruvbox-contrib, spacemacs, spacemacs-neuron }: {
     nixosConfigurations.isaac-nixos-desktop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
@@ -21,7 +31,10 @@
               projectRoot = ./.;
               settings = config.settings;
               inputs = {
-                inherit gruvbox-contrib;
+                inherit
+                  gruvbox-contrib
+                  spacemacs
+                  spacemacs-neuron;
               };
             };
         })
@@ -31,7 +44,6 @@
         ./users/isaac
         # Disabled to debug slow build times
         # ./users/work
-
       ];
     };    
   };
--- home/emacs/default.nix
+++ home/emacs/default.nix
@@ -1,26 +1,15 @@
+{ inputs, ... }:
 {
   programs.emacs.enable = true;
   
   home.file.".emacs.d" = {
     recursive = true;
-    source = builtins.fetchGit {
-      name = "spacemacs";
-      url = https://github.com/syl20bnr/spacemacs/;
-      ref = "refs/heads/develop";
-      # `git ls-remote https://github.com/syl20bnr/spacemacs develop`
-      rev = "6774f37fc1c542fab664f1b65931e52a4991d71a";
-    };
+    source = inputs.spacemacs;
   };
   
   home.file.".emacs.d/private/spacemacs-neuron" = {
     recursive = true;
-    source = builtins.fetchGit {
-      name = "spacemacs-neuron";
-      url = https://github.com/LightAndLight/spacemacs-neuron/;
-      ref = "refs/heads/1.9.33.0";
-      # `git ls-remote https://github.com/LightAndLight/spacemacs-neuron master`
-      rev = "3750b3e7793f50e11ffac3aa0425a56fc279d5ab";
-    };
+    source = inputs.spacemacs-neuron;
   };
   
   home.file.".spacemacs" = {
