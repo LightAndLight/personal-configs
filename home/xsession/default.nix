ec266384a91448027eff38f4adcf3bf018defb12
Isaac Elliott <isaace71295@gmail.com>
2024-08-03 13:39:46 +1000

    Use picom compositor for rounded window borders

--- home/xsession/default.nix
+++ home/xsession/default.nix
@@ -9,5 +9,16 @@
     initExtra = with pkgs; ''
       ${xorg.xset}/bin/xset -b
     '';
-  };  
+  };
+
+  services.picom = {
+    enable = true;
+
+    # Required for properly rounded corners
+    backend = "glx";
+
+    settings = {
+      corner-radius = 10;
+    };
+  };
 }
