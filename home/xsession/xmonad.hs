1e24634b16eb50693b5d825859caba72bdbe1497
Isaac Elliott <isaace71295@gmail.com>
2024-08-03 13:41:25 +1000

    Refactor xmonad config

--- home/xsession/xmonad.hs
+++ home/xsession/xmonad.hs
@@ -8,37 +8,37 @@ import XMonad.Layout.WindowNavigation
 import XMonad.Util.EZConfig
 import XMonad.Hooks.EwmhDesktops (ewmh)
 
-cfg =
-  docks $
+main =
+  xmonad .
+  (\cfg ->
+    cfg
+      { normalBorderColor = "#ebdbb2"
+      , focusedBorderColor = "#458588"
+      , borderWidth = 4
+      , modMask = mod4Mask
+      , layoutHook =
+          spacingRaw False (Border 10 0 10 0) True (Border 0 10 0 10) True .
+          windowNavigation .
+          avoidStruts $
+          layoutHook cfg
+      }
+  ) .
+  docks .
+  (`additionalKeys`
+    [ ((mod4Mask, xK_Return), spawn "alacritty")
+    , ((mod4Mask, xK_p), spawn "exe=`dmenu_path | dmenu -fn Hack -nb '#282828' -nf '#ebdbb2' -sb '#458588' -sf '#ebdbb2' -i` && exec $exe")
+    -- `scrap` is a screen capture script, defined elsewhere in this repo.
+    , ((mod4Mask, xK_s), spawn "scrap")
+    , ((mod4Mask .|. controlMask, xK_l), spawn "xscreensaver-command -lock")
+    , ((mod4Mask, xK_h), sendMessage $ Go L)
+    , ((mod4Mask, xK_j), sendMessage $ Go D)
+    , ((mod4Mask, xK_k), sendMessage $ Go U)
+    , ((mod4Mask, xK_l), sendMessage $ Go R)
+    , ((mod4Mask .|. shiftMask, xK_h), sendMessage $ Swap L)
+    , ((mod4Mask .|. shiftMask, xK_j), sendMessage $ Swap D)
+    , ((mod4Mask .|. shiftMask, xK_k), sendMessage $ Swap U)
+    , ((mod4Mask .|. shiftMask, xK_l), sendMessage $ Swap R)
+    ]
+  ) .
+  ewmh $
   def
-  { normalBorderColor = "#ebdbb2"
-  , focusedBorderColor = "#458588"
-  , borderWidth = 4
-  , modMask = mod4Mask
-  , manageHook =
-      manageDocks <+>
-      manageHook def
-  , layoutHook =
-      spacingRaw False (Border 10 0 10 0) True (Border 0 10 0 10) True .
-      windowNavigation .
-      avoidStruts $
-      layoutHook def
-  , handleEventHook = handleEventHook def
-  }
-  `additionalKeys`
-  [ ((mod4Mask, xK_Return), spawn "alacritty")
-  , ((mod4Mask, xK_p), spawn "exe=`dmenu_path | dmenu -fn Hack -nb '#282828' -nf '#ebdbb2' -sb '#458588' -sf '#ebdbb2' -i` && exec $exe")
-  -- `scrap` is a screen capture script, defined elsewhere in this repo.
-  , ((mod4Mask, xK_s), spawn "scrap")
-  , ((mod4Mask .|. controlMask, xK_l), spawn "xscreensaver-command -lock")
-  , ((mod4Mask, xK_h), sendMessage $ Go L)
-  , ((mod4Mask, xK_j), sendMessage $ Go D)
-  , ((mod4Mask, xK_k), sendMessage $ Go U)
-  , ((mod4Mask, xK_l), sendMessage $ Go R)
-  , ((mod4Mask .|. shiftMask, xK_h), sendMessage $ Swap L)
-  , ((mod4Mask .|. shiftMask, xK_j), sendMessage $ Swap D)
-  , ((mod4Mask .|. shiftMask, xK_k), sendMessage $ Swap U)
-  , ((mod4Mask .|. shiftMask, xK_l), sendMessage $ Swap R)
-  ]
-
-main = xmonad $ ewmh cfg
