8a8b7a7a0d095bb39b5c2b37952d7e280b2cc9bf
Isaac Elliott <isaace71295@gmail.com>
2024-11-10 08:43:10 +1000

    Disable Firefox ads

--- home/firefox/profile.nix
+++ home/firefox/profile.nix
@@ -1,6 +1,19 @@
 {
   settings = {
     "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+
+    # Disable built-in ads
+    "browser.newtabpage.activity-stream.discoverystream.newSponsoredLabel.enabled" = false;
+    "browser.newtabpage.activity-stream.discoverystream.sponsored-collections.enabled" = false;
+    "browser.newtabpage.activity-stream.showSponsored" = false;
+    "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+    "browser.newtabpage.activity-stream.system.showSponsored" = false;
+    "browser.urlbar.quicksuggest.impressionCaps.sponsoredEnabled" = false;
+    "browser.urlbar.quicksuggest.sponsoredIndex" = -1;
+    "browser.urlbar.sponsoredTopSites" = false;
+    "browser.urlbar.suggest.quicksuggest.sponsored" = false;
+    "services.sync.prefs.sync.browser.newtabpage.activity-stream.showSponsored" = false;
+    "services.sync.prefs.sync.browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
   };
   userChrome = ''
     #TabsToolbar { visibility: collapse; }
