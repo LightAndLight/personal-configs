de90ea2134cbc837c9725f975815e83736cb8c9f
Isaac Elliott <isaace71295@gmail.com>
2024-12-12 09:01:54 -0600

    Update to NixOS 24.11

--- system/sound.nix
+++ system/sound.nix
@@ -1,5 +1,13 @@
 { pkgs, ... }: {
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-  environment.systemPackages = [ pkgs.pavucontrol ];
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  environment.systemPackages = [
+    # https://discourse.nixos.org/t/no-audio-devices-with-pipewire-on-lenovo-x1-carbon-7th-gen/38966/6
+    pkgs.sof-firmware
+
+    pkgs.pavucontrol
+  ];
 }
