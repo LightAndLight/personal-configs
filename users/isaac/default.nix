26a07e0fa78645f732454142b8277f8883d69adc
Isaac Elliott <isaace71295@gmail.com>
2024-12-15 10:26:10 -0600

    Set up Thunderbird for personal email

--- users/isaac/default.nix
+++ users/isaac/default.nix
@@ -17,17 +17,17 @@
       userEmail = "isaace71295@gmail.com";
     };
 
-    accounts.email.accounts.isaac = {
-      primary = true;
-      realName = "Isaac Elliott";
-      address = "isaace71295@gmail.com";
-      thunderbird = {
-        enable = true;
-        settings = {
-          # https://support.mozilla.org/en-US/kb/customize-date-time-formats-thunderbird#w_create-date-and-time-format-override-preferences-using-thunderbirds-config-editor
-          "intl.date_time.pattern_override.date_short" = "yyyy-MM-dd";
-          "intl.date_time.pattern_override.time_short" = "HH:mm";
-        };
+    programs.thunderbird = {
+      enable = true;
+      profiles.default.isDefault = true;
+      settings = {
+        # https://support.mozilla.org/en-US/kb/customize-date-time-formats-thunderbird#w_create-date-and-time-format-override-preferences-using-thunderbirds-config-editor
+        "intl.date_time.pattern_override.date_short" = "yyyy-MM-dd";
+        "intl.date_time.pattern_override.time_short" = "HH:mm";
+        "mail.biff.play_sound" = false;
+        "mail.biff.show_alert" = false;
+        "mail.chat.play_sound" = false;
+        "mail.chat.show_desktop_notifications" = false;
       };
     };
   };
