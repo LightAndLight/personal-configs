fd1f05837980ee5e0125f44e8f9742264b2dd7d9
Isaac Elliott <isaace71295@gmail.com>
2024-12-18 10:56:42 +1000

    Silence scrot

--- system/packages/scrap.nix
+++ system/packages/scrap.nix
@@ -3,5 +3,5 @@ writeScriptBin "scrap" ''
   #! ${bash}/bin/bash
 
   mkdir -p "$HOME"/screenshots
-  ${scrot}/bin/scrot -F "$HOME"'/screenshots/%Y-%m-%dT%H:%M:%S_scrot_$wx$h.png' --format png -s -f
+  ${scrot}/bin/scrot -z -F "$HOME"'/screenshots/%Y-%m-%dT%H:%M:%S_scrot_$wx$h.png' --format png -s -f
 ''
