8d4307cfc5b3ac2cc0ef90172616fe200977ade8
Isaac Elliott <isaace71295@gmail.com>
2025-01-25 19:22:07 +1000

    Improve keepassxc standalone and daemon setup

--- home/keepassxc.nix
+++ home/keepassxc.nix
@@ -9,15 +9,24 @@
 #   MinimizeToTray=true
 #   ```
 #
-# * The "Quit" (Ctrl+Q) action in the UI ends the daemon. Keepass windows
-#   need to be closed without "Quit"ting for the daemon to remain active.
-#
 # * The browser extension can pop up a window to request a password for
 #   the database, but if you close that window without unlocking then
 #   that window won't reappear. You have to restart the daemon to make
 #   it work again.
 
 let
+  commonConfig = ''
+    [PasswordGenerator]
+    AdditionalChars=
+    ExcludedChars=
+    Length=25
+    LowerCase=true
+    SpecialChars=true
+    UpperCase=true
+  '';
+
+  # The `keepassxc` daemon is used by browser extensions (`[Browser].Enabled = true`).
+  # It's not intended to have a GUI, so it's started minimised and remains that way.
   daemonConfig = pkgs.writeText "keepassxc.ini" ''
     [General]
     ConfigVersion=2
@@ -34,38 +43,33 @@ let
     MinimizeOnClose=true
     MinimizeOnStartup=true
 
-    [PasswordGenerator]
-    AdditionalChars=
-    ExcludedChars=
-    Length=25
-    LowerCase=true
-    SpecialChars=true
-    UpperCase=true
+    ${commonConfig}
   '';
-in
-{
-  xdg.configFile."keepassxc/keepassxc.ini".text = ''
+
+  # Standalone `keepassxc` instances are started using the launcher or CLI.
+  # They are independent (`[General].SingleInstance=false`) and don't minimise.
+  # They have no browser integration (`[Browser].Enabled = false`).
+  standaloneConfig = pkgs.writeText "keepassxc.ini" ''
     [General]
     ConfigVersion=2
-    SingleInstance=true
+    SingleInstance=false
 
     [Browser]
     CustomProxyLocation=
-    Enabled=true
+    Enabled=false
 
     [GUI]
     TrayIconAppearance=monochrome-light
+    ShowTrayIcon=false
+    MinimizeToTray=false
     MinimizeOnClose=false
     MinimizeOnStartup=false
 
-    [PasswordGenerator]
-    AdditionalChars=
-    ExcludedChars=
-    Length=25
-    LowerCase=true
-    SpecialChars=true
-    UpperCase=true
+    ${commonConfig}
   '';
+in
+{
+  xdg.configFile."keepassxc/keepassxc.ini".source = standaloneConfig;
 
   systemd.user.services.keepassxc = {    
     Unit = {
@@ -79,9 +83,17 @@ in
       Type = "dbus";
       # https://github.com/keepassxreboot/keepassxc/wiki/Using-DBus-with-KeePassXC
       BusName = "org.keepassxc.KeePassXC.MainWindow";
-      Environment = ''
-        KPXC_CONFIG=${daemonConfig}
-      '';
+      Environment =
+        pkgs.lib.strings.concatMapStringsSep " " (x: "\"${x}\"")
+          [
+            "KPXC_CONFIG=${daemonConfig}"
+
+            # Ensure dialogs have the correct theme and scaling.
+            #
+            # See also: `system/display.nix`
+            "QT_QPA_PLATFORMTHEME=qt5ct"
+            "QT_SCALE_FACTOR_ROUNDING_POLICY=PassThrough"
+          ];
       ExecStart = "${pkgs.keepassxc}/bin/keepassxc";
       Restart = "on-failure";
       RestartSec = "2s";
