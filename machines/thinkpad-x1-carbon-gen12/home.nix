7d05906f2affcbd32ae5968d92445f3ace6a6c29
Isaac Elliott <isaace71295@gmail.com>
2025-08-23 11:03:10 +1000

    Set perceptually uniform pixel scaling in Thunderbird

--- machines/thinkpad-x1-carbon-gen12/home.nix
+++ machines/thinkpad-x1-carbon-gen12/home.nix
@@ -1,9 +1,24 @@
 { lib, ... }: {
-  options.programs.firefox.profiles = lib.mkOption {
-    type = lib.types.attrsOf (lib.types.submodule ({ config, ... }: {
-      config.settings = {
-        "layout.css.devPixelsPerPx" = 0.846;
+  options = {
+    programs = {
+      firefox.profiles = lib.mkOption {
+        type = lib.types.attrsOf (lib.types.submodule ({ config, ... }: {
+          config.settings = {
+            "layout.css.devPixelsPerPx" = 0.846;
+          };
+        }));
       };
-    }));
+
+      thunderbird.profiles = lib.mkOption {
+        type = lib.types.attrsOf (lib.types.submodule ({ config, ... }: {
+          config.settings = {
+            "layout.css.devPixelsPerPx" =
+              # Thunderbird config parsing has a bug: it reports a syntax error
+              # if we use a decimal literal.
+              builtins.toString 0.846;
+          };
+        }));
+      };
+    };
   };
 }
