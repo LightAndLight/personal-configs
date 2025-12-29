5ebee025f1e0452f621cec0a22fbbca244a8a0fb
Isaac Elliott <isaace71295@gmail.com>
2025-12-30 08:18:29 +1000

    Disable global Docker setting

--- machines/thinkpad-x1-carbon-gen12/default.nix
+++ machines/thinkpad-x1-carbon-gen12/default.nix
@@ -29,6 +29,8 @@
     };
   };
 
+  virtualisation.podman.enable = true;
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
--- system/virtualisation.nix
+++ system/virtualisation.nix
@@ -1,6 +1,4 @@
 {
-  virtualisation.docker.enable = false;
-
   virtualisation.virtualbox.host.enable = false;
   users.extraGroups.vboxusers.members = [ "user-with-access-to-virtualbox" ];
 }
