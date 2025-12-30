32f8ef15c73933bf9c38ca74ac78fa900b8ac2d8
Isaac Elliott <isaace71295@gmail.com>
2025-12-30 16:36:26 +1000

    Refactor Syncthing config

--- system/sync.nix
+++ system/sync.nix
@@ -1,4 +1,7 @@
-{ config, inputs, pkgs, ...}:
+{ config, inputs, pkgs, ... }:
+let
+  syncthingApiKeyFile = "/run/secrets/syncthing-api-key";
+in
 {
   imports = [
     inputs.asker.nixosModules.nixos
@@ -6,7 +9,36 @@
     inputs.syncthing-merge.nixosModules.default
   ];
 
-  services.syncthing.enable = true;
+  services.syncthing = {
+    enable = true;
+    overrideDevices = false;
+    overrideFolders = false;
+    settings.folders = {
+      "~/sync" = {
+        id = "pkf7r-yzgxs";
+        type = "sendreceive";
+      };
+    };
+  };
+
+  # Allow reading by `syncthing` group members
+  users.users.syncthing.homeMode = "770";
+
+  # Grant explicit access to the `syncthing` user and `syncthing` group members.
+  #
+  # ## Motivation
+  #
+  # I sync a KeePassXC database from my desktop to other devices. When I edit the database,
+  # KeePassXC sets the user, group, and permissions to the defaults for the account that
+  # I was using. This denies access to the `syncthing` user. Also, I use separate work
+  # and personal accounts on the same machine. If I edit the database from one account
+  # then change to the other then the second account will be denied access.
+  #
+  # I work around this by having all my accounts in the `syncthing` group, and granting
+  # explicit access to `syncthing` group members.
+  systemd.tmpfiles.rules = [
+    "A /var/lib/syncthing - - - - u:syncthing:rwX,d:u:syncthing:rwX,g:syncthing:rwX,d:g:syncthing:rwX"
+  ];
 
   sops.secrets.syncthing-api-key = {
     sopsFile = ./sync/api-key.txt.secret;
@@ -15,7 +47,7 @@
     mode = "0440";
     owner = config.services.syncthing.user;
     group = config.services.syncthing.group;
-    restartUnits = ["syncthing.service"];
+    restartUnits = ["syncthing.service" "syncthing-merge.service"];
   };
 
   services.asker = {
@@ -30,7 +62,7 @@
     extraGroups = [ config.services.asker.keys.keepassxc.group ];
     settings = {
       syncthingUrl = "http://sync.local";
-      syncthingApiKeyFile = "/run/secrets/syncthing-api-key";
+      inherit syncthingApiKeyFile;
       mergeTargets = [
         {
           folderId = "pkf7r-yzgxs";
@@ -63,7 +95,6 @@
   systemd.services.syncthing.serviceConfig.ExecStartPre =
     let
       syncthingConfig = "${config.services.syncthing.configDir}/config.xml";
-      syncthingApiKeyPath = "/run/secrets/syncthing-api-key";
     in
       pkgs.writeScript "syncthing-set-key" ''
         #! ${pkgs.python3}/bin/python3
@@ -71,7 +102,7 @@
         import os
         import re
 
-        secret_path = "${syncthingApiKeyPath}"
+        secret_path = "${syncthingApiKeyFile}"
         print(f"Setting Syncthing API key from {secret_path}")
 
         old_path = "${syncthingConfig}"
@@ -87,25 +118,6 @@
 
         os.rename(new_path, old_path)
       '';
-
-  # Allow reading by `syncthing` group members
-  users.users.syncthing.homeMode = "770";
-
-  # Grant explicit access to the `syncthing` user and `syncthing` group members.
-  #
-  # ## Motivation
-  #
-  # I sync a KeePassXC database from my desktop to other devices. When I edit the database,
-  # KeePassXC sets the user, group, and permissions to the defaults for the account that
-  # I was using. This denies access to the `syncthing` user. Also, I use separate work
-  # and personal accounts on the same machine. If I edit the database from one account
-  # then change to the other then the second account will be denied access.
-  #
-  # I work around this by having all my accounts in the `syncthing` group, and granting
-  # explicit access to `syncthing` group members.
-  systemd.tmpfiles.rules = [
-    "A /var/lib/syncthing - - - - u:syncthing:rwX,d:u:syncthing:rwX,g:syncthing:rwX,d:g:syncthing:rwX"
-  ];
 
   services.nginx = {
     enable = true;
